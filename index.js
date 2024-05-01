35bf06417eb78249ddc6cf56de9ad99edbcc623c
AntonioWSousa <antoniowsj@outlook.com>
2024-05-01 16:54:04 -0300

    Commit enviado

---
+++ index.js
@@ -0,0 +1,23 @@
+let nomeHeroi = "Oskar"
+let xpHeroi = "8001"
+let nivel = ""
+
+if (xpHeroi <= 1000) {
+  nivel = "Ferro"
+} else if (xpHeroi <= 2000) {
+  nivel = "Bronze"
+} else if (xpHeroi <= 6000) {
+  nivel = "Prata"
+} else if (xpHeroi <= 7000) {
+  nivel = "Ouro"
+} else if (xpHeroi <= 8000) {
+  nivel = "Platina"
+} else if (xpHeroi <= 9000) {
+  nivel = "Ascendente"
+} else if (xpHeroi <= 10000) {
+  nivel = "Imortal"
+} else if (xpHeroi >= 10001) {
+  nivel = "Radiante"
+}
+
+console.log(`O Herói, ${nomeHeroi}, está no nível ${nivel}.`)
